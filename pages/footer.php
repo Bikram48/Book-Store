7e68bd08e0a6200eb801f8d136945e022d4e0247
Bikram Chandf <chandbikram001@gmail.com>
2021-02-24 08:12:26 +0545

    bookstore website

--- pages/footer.php
+++ pages/footer.php
@@ -11,43 +11,30 @@
 </head>
 
 <body>
-    <!-- Footer -->
 <footer class="bg-dark text-center text-white">
-  <!-- Grid container -->
   <div class="container p-4">
-    <!-- Section: Social media -->
     <section class="mb-4">
-      <!-- Facebook -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-facebook-f"></i
       ></a>
-
-      <!-- Twitter -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-twitter"></i
       ></a>
-
-      <!-- Google -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-google"></i
       ></a>
 
-      <!-- Instagram -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-instagram"></i
       ></a>
-      <!-- Github -->
+
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-github"></i
       ></a>
     </section>
-    <!-- Section: Social media -->
-
-    <!-- Section: Links -->
+  
     <section class="">
-      <!--Grid row-->
       <div class="row">
-        <!--Grid column-->
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
     
           <ul class="list-unstyled mb-0">
@@ -56,9 +43,7 @@
             </li>
           </ul>
         </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
+  
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
     
           <ul class="list-unstyled mb-0">
@@ -67,9 +52,7 @@
             </li>
           </ul>
         </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
+      
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
     
           <ul class="list-unstyled mb-0">
@@ -78,9 +61,7 @@
             </li>
           </ul>
         </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
+    
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
     
           <ul class="list-unstyled mb-0">
@@ -89,20 +70,14 @@
             </li>
           </ul>
         </div>
-        <!--Grid column-->
       </div>
-      <!--Grid row-->
     </section>
-    <!-- Section: Links -->
   </div>
-  <!-- Grid container -->
 
-  <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
+    © 2021 Copyright:
     <a class="text-white" href="https://Bookland.com/">Bookland.com</a>
   </div>
-  <!-- Copyright -->
 </footer>
 </body>
 </html>
